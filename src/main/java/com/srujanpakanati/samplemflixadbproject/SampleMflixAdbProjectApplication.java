cd00611d0b9a47f244ac0379609aeb725f19657e
HighonAces <srjnreddy33@gmail.com>
2022-02-06 13:57:19 -0600

    Initial commit

---
+++ src/main/java/com/srujanpakanati/samplemflixadbproject/SampleMflixAdbProjectApplication.java
@@ -0,0 +1,13 @@
+package com.srujanpakanati.samplemflixadbproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleMflixAdbProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleMflixAdbProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/srujanpakanati/samplemflixadbproject/SampleMflixAdbProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srujanpakanati.samplemflixadbproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleMflixAdbProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
